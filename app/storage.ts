81d3b9fdbba8005ab2ab1273020b9fc18fb9bb6c
Maximo Dominguez <mamodom@gmail.com>
2019-09-05 19:13:27 -0400

    using env

--- app/storage.ts
+++ app/storage.ts
@@ -2,13 +2,13 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 
 firebase.initializeApp({
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 });
 
 const db = firebase.firestore();
