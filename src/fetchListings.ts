872fd4191007cf7f028800b2099bf94febbfb436
Maximo Dominguez <mamodom@gmail.com>
2019-09-05 20:41:37 -0400

    Adding search tags to avoid listings from other cities

--- src/fetchListings.ts
+++ src/fetchListings.ts
@@ -4,6 +4,32 @@ import IRentalProperty from "./IRentalProperty";
 
 export default async () => {
   const filter = {
+    SearchTags: [
+      {
+        Name: "Stockholm Södermalm (område)",
+        Url: "stockholm-sodermalm",
+        Identifier: null,
+        Type: 0
+      },
+      {
+        Name: "Stockholm City (område)",
+        Url: "stockholm-city",
+        Identifier: null,
+        Type: 0
+      },
+      {
+        Name: "Stockholm (län)",
+        Url: "stockholm",
+        Identifier: null,
+        Type: 0
+      },
+      {
+        Name: "Stockholm (kommun)",
+        Url: "stockholm/stockholm",
+        Identifier: null,
+        Type: 0
+      }
+    ],
     Culture: "en-US",
     ObjectsPerPage: 10,
     Page: 1,
--- src/fetchMarkers.ts
+++ src/fetchMarkers.ts
@@ -3,6 +3,32 @@ import IMarker from "./IMarker";
 
 const fetchMarkers = async () => {
   const filter = {
+    SearchTags: [
+      {
+        Name: "Stockholm Södermalm (område)",
+        Url: "stockholm-sodermalm",
+        Identifier: null,
+        Type: 0
+      },
+      {
+        Name: "Stockholm City (område)",
+        Url: "stockholm-city",
+        Identifier: null,
+        Type: 0
+      },
+      {
+        Name: "Stockholm (län)",
+        Url: "stockholm",
+        Identifier: null,
+        Type: 0
+      },
+      {
+        Name: "Stockholm (kommun)",
+        Url: "stockholm/stockholm",
+        Identifier: null,
+        Type: 0
+      }
+    ],
     Culture: "en-US",
     ObjectsPerPage: 10,
     Page: 1,
